a8ca7f997732cba7182c028416ca2b3935f8fda9
Richard Jarrell <richardjarrell@yahoo.com>
2019-09-20 19:51:05 -0500

    semicolons and tabs

--- dbConnection.js
+++ dbConnection.js
@@ -1,13 +1,13 @@
-require('dotenv').config()
-const { Client } = require('pg')
+require('dotenv').config();
+const { Client } = require('pg');
 
 // setup connection to postgres
 module.exports = {
-  db: new Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DB_NAME,
-    password: process.env.PG_PW,
-    port: process.env.PG_PORT
-  })
-}
+    db: new Client({
+        user: process.env.PG_USER,
+        host: process.env.PG_HOST,
+        database: process.env.PG_DB_NAME,
+        password: process.env.PG_PW,
+        port: process.env.PG_PORT
+    })
+};
