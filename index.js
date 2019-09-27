63ad2dac16927b086d0bb3063b81efb9111f32e4
Richard Jarrell <richardjarrell@yahoo.com>
2019-09-27 11:42:57 -0500

    fix missing /we line

--- index.js
+++ index.js
@@ -177,6 +177,7 @@ app.post('/test', (req, res) => {
     console.log(`SlashCommand: ${slashCommand}`);
     switch (slashCommand) {
         case '/ww':
+        case '/we':
         case '/werewolf':
             const requestBody = req.body;
             console.log(`Request Body: ${requestBody}`);
